ed1c999dd94479631f490f282af9c37e788594dd
Emy <emilly.r.nascimento6@aluno.senai.br>
2025-10-22 13:13:11 -0300

    FORMULÁRIO DEU CERTOOOOOOO

--- server.js
+++ server.js
@@ -11,27 +11,29 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 //Rota para a página inicial (index.html)
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Rota para envio do formulário
-app.post("/submit-form", (req,res) => {
-    const dadosForm = req.body;
-    const nomeArquivo = `formulario-${dadosForm.nome}.json`;
-    const caminhoArquivo = path.join(__dirname, "formularios", nomeArquivo);
+app.post("/submit-form", (req, res) => {
+  const dadosForm = req.body;
+  const nomeArquivo = `formulario-${dadosForm.nome}.json`;
+  const caminhoArquivo = path.join(__dirname, "formularios", nomeArquivo);
 
-    fs.writeFile(caminhoArquivo, JSON.stringify(dadosForm, null, 2), (error) => {
-        if(error){
-            console.error("Erro ao salvar o arquivo:", error);
-            return res.status(500).send("Erro ao salvar o formulário");
-        }
-        res.status(200).send("Fromulário salvo com sucesso!");
-    });
+  fs.writeFile(caminhoArquivo, JSON.stringify(dadosForm, null, 2), (error) => {
+    if (error) {
+      console.error("Erro ao salvar o arquivo:", error);
+      return res.status(500).send("Erro ao salvar o formulário");
+    }
+    res.status(200).send("Fromulário salvo com sucesso!");
+  });
 });
+
+console.log("!!! ALERTA: A ROTA /submit-form FOI ACIONADA !!!");
 
 // Iniciar o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}.`);
-})
+  console.log(`Servidor rodando na porta ${PORT}.`);
+});
